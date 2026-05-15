15ce99dddfece6f0f2d8cb0a6ba2544eb514ad95
Lucas Yang <0965247@outlook.com>
2026-05-14 17:17:16 -0700

    Added Description of program so it is easier to understand. Also added nessesary directives in order for the program to work.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 // Author: Lucas Yang
 
 /* 
- * Description:
- *
+ * Description:This program will allow the user to be able to track the amount of video games and their titles,
+ * how long they have played, the cost of each game, and the rating of the game.
 */
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main() {
     return 0;
